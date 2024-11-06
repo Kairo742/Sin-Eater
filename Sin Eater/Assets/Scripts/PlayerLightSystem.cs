ec11331d940379dbf78b23f929840d91a673ef1b
ChandlerF <63360113+ChandlerF@users.noreply.github.com>
2024-11-06 01:49:13 -0600

    Player Light Working and Decaying

---
+++ Sin Eater/Assets/Scripts/PlayerLightSystem.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class PlayerLightSystem : MonoBehaviour
+{
+    public float LightAmount = 0f;          //Best Values are : 1 - 20
+    public float PassiveLightDecay = 0.5f;      //Minus this value from Light Amount every second
+
+
+    [SerializeField] private Light _playerLight;
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        if (LightAmount > 0f)
+        {
+            LightAmount -= (PassiveLightDecay * Time.deltaTime);    //Light Decay
+            _playerLight.intensity = LightAmount + 0.5f;
+        }
+    }
+}
